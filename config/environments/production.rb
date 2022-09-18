4a939f6dd888c8772ff1dd9290222bcb8f57e96b
asolab <asolabofficial@gmail.com>
2022-09-18 07:57:52 +0100

    added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,14 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'superails.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'asolabrails.herokuapp.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
-  domain:               'mail.google.com',
+  domain:               'asolabrails.herokuapp.com',
   user_name:            Rails.application.credentials.dig(:google_smtp, :email),
   password:             Rails.application.credentials.dig(:google_smtp, :password),
   authentication:       'plain',
@@ -32,7 +33,7 @@ config.action_mailer.smtp_settings = {
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
