e5e8466a3d19d0b7ef30deaa870ecee824270d09
asolab <asolabofficial@gmail.com>
2022-10-04 14:59:32 +0100

    sender updated

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Asolab-Rails <asolab@hausasports.art>'
+  config.mailer_sender = 'FriendsApp-Rails <asolab@hausasports.art>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
